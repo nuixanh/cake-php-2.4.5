4296deb4745438cd651b7f03ff768d9f317ffdce
Son Nguyen Thanh <nuixanh_ptit@yahoo.com>
2014-02-26 14:03:30 +0700

    follow redirect url

--- app/hitFunctions.php
+++ app/hitFunctions.php
@@ -7,6 +7,23 @@
  * To change this template use File | Settings | File Templates.
  */
 
+function get_hit($site){
+    $ch = curl_init($site->url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 15);//The maximum number of seconds to allow cURL functions to execute.
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 15);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_exec($ch);
+    $info = null;
+    if(!curl_errno($ch))
+    {
+        $info = curl_getinfo($ch);
+//        echo 'Took ' . $info['total_time'] . ' seconds to send a request to ' . $info['url'] . "\n";
+    }
+
+    curl_close($ch);
+    return $info;
+}
 function multiple_threads_hit($sites)
 {
     $mh = curl_multi_init();//Returns a cURL multi handle resource
@@ -25,25 +42,42 @@ function multiple_threads_hit($sites)
     do {
         usleep(10000);
         curl_multi_exec($mh, $running);
-        $info = curl_multi_info_read($mh);
-        if (false !== $info) {
-            //var_dump($info);
-            $url = curl_getinfo($info['handle'], CURLINFO_EFFECTIVE_URL);
+//        curl_multi_select($mh);
+        $info = curl_multi_info_read($mh, $msgs_in_queue);
+//        echo "info" . $info . "\t\trunning = ". $running . "\n";
+//        echo "$msgs_in_queue msgs_in_queue\t\trunning = ". $running . "\n";
+//        if($info === FALSE){
+//            echo "----------------no more to get at this point \t\trunning = ". $running . "\n";
+//        }
+        if (FALSE !== $info) {
+//            $url = curl_getinfo($info['handle'], CURLINFO_EFFECTIVE_URL);
             $ch_info = curl_getinfo($info['handle']);
+            $url = $ch_info['url'];
+            $http_code = $ch_info['http_code'];
+//            echo $ch_info['http_code'] . "\n";
+            echo $url . "\t\t". $ch_info['http_code'] . "\n";
+            if($http_code === 301){//re-direct
+                $site = new stdClass();
+                $site->url = $url;
+                $ch_info = get_hit($site);
+                $ch_info['redirect'] = 1;
+            }else{
+                $ch_info['redirect'] = 0;
+            }
 //            print_r($ch_info);
 
-            foreach ($sites as $i => $site) {
+            foreach ($sites as $site) {
                 if($site->url === $url){
                     $site->http_code = $ch_info['http_code'];
                     $site->connect_time = $ch_info['connect_time'];
                     $site->total_time = $ch_info['total_time'];
                     $site->primary_ip = $ch_info['primary_ip'];
+                    $site->redirect = $ch_info['redirect'];
                 }
             }
-//            echo curl_getinfo($info['handle'],CURLINFO_EFFECTIVE_URL) . "\n";
-//            echo curl_getinfo($info['handle'],CURLINFO_TOTAL_TIME ) . " seconds \n";
+
         }
-    } while ($running > 0);
+    } while ($running > 0 || $msgs_in_queue > 0);
 
     foreach ($sites as $i => $url) {
         curl_multi_remove_handle($mh, $curl_array[$i]);
--- app/hitSites.php
+++ app/hitSites.php
@@ -31,36 +31,51 @@ if ($db_found) {
     $from = $now->format('Y-m-d H:i:s');
     $now->add(new DateInterval('PT60M'));
     $to = $now->format('Y-m-d H:i:s');
-    $SQL = "SELECT * FROM sites where active=true and next_monitor_time >= '" . $from . "' and next_monitor_time < '" . $to . "'";
-    print $SQL;
+    $SQL = "SELECT * FROM sites where active=true and next_monitor_time >= '" . $from . "' and next_monitor_time < '" . $to . "' order by last_response_time";
+    print $SQL . "\n";
     $result = mysql_query($SQL);
 
     $sites=array();
+    $arr_of_arr=array();
     $total=0;
+    $i=0;
     while ( $db_field = mysql_fetch_assoc($result) ) {
+        if($i % $no_of_threads === 0){
+            $sites=array();
+            array_push($arr_of_arr, $sites);
+        }
         $site = new stdClass();
         $site->id = $db_field['id'];
         $site->url = $db_field['url'];
         $site->interval = $db_field['interval'];
-//        var_dump($site);
-        $total=array_push($sites, $site);
+        $j = floor ($i / $no_of_threads);
+        $total=array_push($arr_of_arr[$j], $site);
+        $i++;
     }
-//    print_r($sites);
+//    print_r($arr_of_arr);
     print "\n--- Total --- " . $total . "\n";
     if($total > 0){
-        $hits = multiple_threads_hit($sites);
-//        print_r($hits);
-        $create = CommonUtil::getMysqlCurrentTime();
-        foreach ($hits as $i => $hit) {
-            $insert_query = "insert INTO hits (id, site_id, url, http_code, connect_time, total_time, primary_ip, created) VALUES ('"
-                . String::uuid() . "', '" . $hit->id . "', '" . $hit->url . "', " . $hit->http_code . ", " . $hit->connect_time
-                . ", " . $hit->total_time . ",'". $hit->primary_ip ."','" . $create . "')";
-            mysql_query($insert_query);
-            $last_monitor_status = ($hit->http_code == 200 || $hit->http_code == 301)? 1: 0;
-            $site_update = "update sites set last_monitor_time = '" . $create . "', next_monitor_time = next_monitor_time + INTERVAL " . $hit->interval
-                . " MINUTE, last_monitor_status = " . $last_monitor_status . " where id = '" . $hit->id ."'";
-            mysql_query($site_update);
+        foreach ($arr_of_arr as $sites) {
+            $hits = multiple_threads_hit($sites);
+//            print_r($hits);
+            $create = CommonUtil::getMysqlCurrentTime();
+            foreach ($hits as $hit) {
+                $insert_query = "insert INTO hits (id, site_id, url, http_code, connect_time, total_time, primary_ip, created, redirect) VALUES ('"
+                    . String::uuid() . "', '" . $hit->id . "', '" . $hit->url . "', " . $hit->http_code . ", " . $hit->connect_time
+                    . ", " . $hit->total_time . ",'". $hit->primary_ip ."','" . $create . "'," . $hit->redirect . ")";
+                mysql_query($insert_query);
+//                $last_monitor_status = ($hit->http_code == 200 || $hit->http_code == 301)? 1: 0;
+                $last_monitor_status = $hit->http_code == 200 ? 1: 0;
+
+//                $site_update = "update sites set last_monitor_time = '" . $create . "', next_monitor_time = next_monitor_time + INTERVAL " . $hit->interval
+//                    . " MINUTE, last_monitor_status = " . $last_monitor_status . ", last_response_time = " . $hit->total_time . " where id = '" . $hit->id ."'";
+                $site_update = "update sites set last_monitor_time = '" . $create . "', next_monitor_time = next_monitor_time + INTERVAL " . 1
+                    . " MINUTE, last_monitor_status = " . $last_monitor_status . ", last_response_time = " . $hit->total_time . " where id = '" . $hit->id ."'";
+                mysql_query($site_update);
+            }
+            print "\n\n";
         }
+
     }
 
     mysql_close($db_handle);
