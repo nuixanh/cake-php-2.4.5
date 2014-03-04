415b26f25fc0a38d494fd2a6679c34b04a7a6fcb
Son Nguyen <son.nguyen.thanh@live.com>
2014-03-05 00:04:39 +0700

    hit firstly after create a new site

--- app/Console/Command/HitShell.php
+++ app/Console/Command/HitShell.php
@@ -9,7 +9,6 @@
 
 App::uses('HitUtil', 'Common');
 App::uses('CommonUtil', 'Common');
-App::uses('CakeLog', 'Log');
 App::uses('WindowsPhonePushNotification', 'Common');
 
 class HitShell extends AppShell
@@ -29,7 +28,8 @@ class HitShell extends AppShell
         $this->Site->read(null, $site_id);
         $url = $this->Site->data['Site']['url'];
         $interval = $this->Site->data['Site']['interval'];
-        $this->out('URL: ' . $url);
+        $user_id = $this->Site->data['Site']['user_id'];
+        $site_name = $this->Site->data['Site']['name'];
         $info = HitUtil::hitSiteByUrl($url);
 
         $this->Site->set('last_monitor_status', $info['http_code'] == 200 ? 1 : 0);
@@ -43,20 +43,20 @@ class HitShell extends AppShell
             'url' => $url,
             'http_code' => $info['http_code'] != null? $info['http_code'] : 0,
             'connect_time' => $info['connect_time'] != null? $info['connect_time'] : 0,
-            'total_time' => $info['total_time'] != null? $info['total_time'] : 0,
+            'total_time' => $info['total_time'] != null? $info['total_time'] : 15,
             'primary_ip' => $info['primary_ip'] != null? $info['primary_ip'] : 0,
             'redirect' => $info['redirect'] != null? $info['redirect'] : 0
         ));
         $this->Hit->save();
         if($info['http_code'] != 200){
             $channels = $this->Channel->find('all', array(
-                'conditions' => array('Channel.user_id' => $this->Site->data['Site']['user_id'])
+                'conditions' => array('Channel.user_id' => $user_id)
             ));
             foreach ($channels as $channel) {
                 $ch_url = $channel['Channel']['url'];
                 $notif = new WindowsPhonePushNotification($ch_url);
-                CakeLog::write('info', $ch_url);
-                $notif->push_toast("DOWN site [" . $this->Site->data['Site']['name'] . ']', " - [" . $this->Site->data['Site']['url'] ."]");
+                $this->out('Channel URL: ' . $ch_url);
+                $notif->push_toast("DOWN site [" . $site_name . ']', "[" . $url ."]");
             }
         }
     }
